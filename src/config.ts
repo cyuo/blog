cbe60094d8d310891cf8332f3580f4d703df340c
cyou <poi6721@outlook.com>
2025-12-19 04:59:23 +0800

    same width banner for PC view

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "/img/demo-banner.png", // Path starting with '/' is relative to the /public directory
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
