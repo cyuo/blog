eea9912fb8e98be917606c6ebbaddcb796eeea12
cyou <poi6721@outlook.com>
2026-01-20 00:31:34 +0800

    change default color

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Yet another Fuwari blog site",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 0, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
